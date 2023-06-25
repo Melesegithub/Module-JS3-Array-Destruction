6d61c0e69157f01270f092716f7732fd5c500926
Melese Berehannu <melemeko12@gmail.com>
2023-06-25 16:31:19 +0100

    JS3-Array-Destruction
    JS3 - Array-Destruction

---
+++ exercise-1/exercise.js
@@ -0,0 +1,13 @@
+const personOne = {
+  name: "Popeye",
+  age: 34,
+  favouriteFood: "Spinach",
+};
+let { name, age, favouriteFood } = personOne;
+function introduceYourself() {
+  console.log(
+    `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
+  );
+}
+
+introduceYourself(personOne);
---
+++ exercise-3/exercise.js
@@ -0,0 +1,17 @@
+let order = [
+  { itemName: "Hot cakes", quantity: 1, unitPrice: 2.29 },
+  { itemName: "Apple Pie", quantity: 2, unitPrice: 1.39 },
+  { itemName: "Egg McMuffin", quantity: 1, unitPrice: 2.8 },
+  { itemName: "Sausage McMuffin", quantity: 1, unitPrice: 3.0 },
+  { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
+  { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
+];
+console.log("QTY    ITEM            TOTAL");
+let total = 0;
+order.forEach((element) => {
+  let individualOrder = element;
+  let { itemName, quantity, unitPrice } = individualOrder;
+  total = total + unitPrice;
+  console.log(`${quantity}`, `     ${itemName}`, `      ${unitPrice}`);
+});
+console.log(`Total:${total}`);
